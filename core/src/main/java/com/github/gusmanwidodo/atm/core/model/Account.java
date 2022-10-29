60802abf35d566d1d186d578014daea51d182003
Gusman Widodo <gusmanwidodo@gmail.com>
2022-10-29 15:41:00 +0800

    Detail account model

--- core/src/main/java/com/github/gusmanwidodo/atm/core/model/Account.java
+++ core/src/main/java/com/github/gusmanwidodo/atm/core/model/Account.java
@@ -1,4 +1,110 @@
 package com.github.gusmanwidodo.atm.core.model;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Account {
+    private long id;
+    private long customerId;
+    private String number;
+    private double balanceAmount;
+    private String status;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    public Account() {
+
+    }
+
+    public Account(long id, long customerId, String number, double balanceAmount, String status, LocalDate createdAt, LocalDate updatedAt) {
+        this.id = id;
+        this.customerId = customerId;
+        this.number = number;
+        this.balanceAmount = balanceAmount;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalanceAmount() {
+        return balanceAmount;
+    }
+
+    public void setBalanceAmount(double balanceAmount) {
+        this.balanceAmount = balanceAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return id == account.id && customerId == account.customerId && Double.compare(account.balanceAmount, balanceAmount) == 0 && Objects.equals(number, account.number) && Objects.equals(status, account.status) && Objects.equals(createdAt, account.createdAt) && Objects.equals(updatedAt, account.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customerId, number, balanceAmount, status, createdAt, updatedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", customerId=" + customerId +
+                ", number='" + number + '\'' +
+                ", balanceAmount=" + balanceAmount +
+                ", status='" + status + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
